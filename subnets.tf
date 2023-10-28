ca6deb3a95b252afda6ae3c470f88e052723dc29
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-10-28 11:00:06 +0200

    deploy subnets

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "private-us-east-1a" {
   vpc_id = aws_vpc.vpc-us-east-1.id
   availability_zone = "us-east-1a"
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "private-subnet-east-1a"
@@ -11,7 +11,7 @@ resource "aws_subnet" "private-us-east-1a" {
 resource "aws_subnet" "private-us-east-1b" {
   vpc_id = aws_vpc.vpc-us-east-1.id
   availability_zone = "us-east-1b"
-  cidr_block = "10.0.2.0/16"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "private-subnet-east-1b"
@@ -21,7 +21,7 @@ resource "aws_subnet" "private-us-east-1b" {
 resource "aws_subnet" "public-us-east-1a" {
   vpc_id = aws_vpc.vpc-us-east-1.id
   availability_zone = "us-east-1a"
-  cidr_block = "10.0.3.0/16"
+  cidr_block = "10.0.3.0/24"
 
   tags = {
     Name = "public-subnet-east-1a"
@@ -31,7 +31,7 @@ resource "aws_subnet" "public-us-east-1a" {
 resource "aws_subnet" "public-us-east-1b" {
   vpc_id = aws_vpc.vpc-us-east-1.id
   availability_zone = "us-east-1b"
-  cidr_block = "10.0.4.0/16"
+  cidr_block = "10.0.4.0/24"
 
   tags = {
     Name = "public-subnet-east-1b"
