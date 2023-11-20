faa085dbfe762845c6eddfcb487e8186d1bb7525
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-11-20 16:31:15 +0100

    123

--- test/ec2_dc.tf
+++ test/ec2_dc.tf
@@ -13,7 +13,7 @@ resource "local_file" "tf-key" {
 
 resource "aws_instance" "dc" {
   instance_type        = "t3.medium"
-  ami                  = data.aws_ami.dc-compan-com.id
+  ami                  = var.dc-compan-com-ami-id
   depends_on           = [aws_key_pair.tf-key-pair]
   key_name             = "tf-key-pair"
   iam_instance_profile = aws_iam_instance_profile.dev-resources-iam-profile.name
