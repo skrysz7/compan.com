ec017623e61b72ab62554dbb22ef26cd1237f446
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-11-20 15:19:09 +0100

    123

--- test/ec2_dc.tf
+++ test/ec2_dc.tf
@@ -1,30 +1,57 @@
 resource "aws_key_pair" "tf-key-pair" {
-key_name = "tf-key-pair"
-public_key = tls_private_key.rsa.public_key_openssh
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits  = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 resource "local_file" "tf-key" {
-content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair"
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
 }
 
 resource "aws_instance" "dc" {
-  instance_type = "t3.medium"
-  ami           = data.aws_ami.win2022.id
-  depends_on    = [aws_key_pair.tf-key-pair]
-  key_name      = "tf-key-pair"
+  instance_type        = "t3.medium"
+  ami                  = data.aws_ami.win2022.id
+  depends_on           = [aws_key_pair.tf-key-pair]
+  key_name             = "tf-key-pair"
+  iam_instance_profile = aws_iam_instance_profile.dev-resources-iam-profile.name
 
-# ignore all changes made manually
-#   lifecycle {
-#     ignore_changes = all
-#   }
+  # ignore all changes made manually
+  #   lifecycle {
+  #     ignore_changes = all
+  #   }
+}
+
+resource "aws_iam_instance_profile" "dev-resources-iam-profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.dev-resources-iam-role.name
+}
+resource "aws_iam_role" "dev-resources-iam-role" {
+  name               = "dev-ssm-role"
+  description        = "The role for the developer resources EC2"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": {
+"Effect": "Allow",
+"Principal": {"Service": "ec2.amazonaws.com"},
+"Action": "sts:AssumeRole"
+}
+}
+EOF
+  tags = {
+    stack = "test"
+  }
+}
+resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
+  role       = aws_iam_role.dev-resources-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_ssm_document" "echo" {
-  depends_on = [aws_instance.dc]
+  depends_on    = [aws_instance.dc]
   name          = "echo"
   document_type = "Command"
   content       = <<DOC
@@ -85,7 +112,7 @@ resource "aws_ssm_association" "example" {
 #         "sudo amazon-linux-extras install -y nginx1.12",
 #         "sudo systemctil start nginx"
 #     ]
-    
+
 #     connection {
 #       type = "ssh"
 #       host = self.public_ip
