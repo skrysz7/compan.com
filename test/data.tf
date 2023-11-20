0bae2b3f3aa1905a1500a28e165ce1e7a814dde7
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-11-20 16:27:40 +0100

    123

--- test/data.tf
+++ test/data.tf
@@ -7,3 +7,11 @@ data "aws_ami" "win2022" {
     values = ["Windows_Server-2022-English-Full-Base-*"]
   }
 }
+
+data "aws_ami" "dc-compan-com" {
+  owners = ["self"]
+  filter {
+    name   = "windows-domain-controller"
+    values = ["windows-domain-controller"]
+  }
+}
