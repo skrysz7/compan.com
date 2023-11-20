e11921e3772ad1693fff771655bd8702ee8e08be
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-11-20 13:55:53 +0100

    deploy infra

--- test/ec2_dc.tf
+++ test/ec2_dc.tf
@@ -1,8 +1,20 @@
+resource "aws_key_pair" "tf-key-pair" {
+key_name = "tf-key-pair"
+public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits  = 4096
+}
+resource "local_file" "tf-key" {
+content  = tls_private_key.rsa.private_key_pem
+filename = "tf-key-pair"
+}
+
 resource "aws_instance" "dc" {
   instance_type = "t3.medium"
   ami           = data.aws_ami.win2022.id
-
-
+  key_name      = "tf-key-pair"
 
 # ignore all changes made manually
 #   lifecycle {
@@ -11,25 +23,32 @@ resource "aws_instance" "dc" {
 }
 
 resource "aws_ssm_document" "echo" {
+  depends_on = [aws_instance.dc]
   name          = "echo"
   document_type = "Command"
   content       = <<DOC
 {
-    "schemaVersion": "1.2",
-    "description": "Add 'Hello' to c:\hello.txt.",
-    "parameters": {
-    },
-    "runtimeConfig": {
-      "aws:runShellScript": {
-        "properties": [
-          {
-            "id": "0.aws:runShellScript",
-            "runCommand": ["echo \"Hello!\" > c:\hello.txt"]
-          }
+  "schemaVersion": "2.2",
+  "description": "Example SSM document",
+  "parameters": {
+    "BucketName": {
+      "type": "String",
+      "description": "The name of the S3 bucket where the PowerShell script is stored."
+    }
+  },
+  "mainSteps": [
+    {
+      "action": "aws:runPowerShellScript",
+      "name": "runPowerShellScript",
+      "inputs": {
+        "runCommand": [
+          "Write-Host 'Running PowerShell script'",
+          "new-item -ItemType Directory C:\test"
         ]
       }
     }
-  }
+  ]
+}
 DOC
 }
 
