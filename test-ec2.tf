6e67e3cc64397e4293b6c64274f322b9cc96ebb3
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-12-20 14:22:12 +0100

    123

--- test-ec2.tf
+++ test-ec2.tf
@@ -1,8 +1,9 @@
 module "ec2-windows" {
   source  = "app.terraform.io/compan/ec2-windows/aws"
-  version = "1.0.4"
+  version = "1.0.7"
 
   hostname = "testsrv456"
   env      = "TEST"
+  kms_key  = aws_kms_alias.kms_alias.name
   #   is_migrated = false
 } 
