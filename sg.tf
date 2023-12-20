42e321103bbeae822b6d69901966ba2c5dc05b5f
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-12-20 15:34:20 +0100

    123

--- sg.tf
+++ sg.tf
@@ -1,24 +1,24 @@
-variable "sg_ports" {
-  type        = list(number)
-  description = "list of ingress ports"
-  default     = [8200, 8300, 443, 80]
-}
+# variable "sg_ports" {
+#   type        = list(number)
+#   description = "list of ingress ports"
+#   default     = [8200, 8300, 443, 80]
+# }
 
-resource "aws_security_group" "dynamicsg" {
-  name        = "dynamic-sg"
-  description = "Ingress for EC2"
+# resource "aws_security_group" "dynamicsg" {
+#   name        = "dynamic-sg"
+#   description = "Ingress for EC2"
 
-  dynamic "ingress" {
-    for_each = var.sg_ports
-    iterator = port
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-}
+#   dynamic "ingress" {
+#     for_each = var.sg_ports
+#     iterator = port
+#     content {
+#       from_port   = port.value
+#       to_port     = port.value
+#       protocol    = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+# }
 
 
 
