077eb4372b4f5c224ba98feea95d2029de07c890
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-12-21 11:37:45 +0100

    123

--- test-ec2.tf
+++ test-ec2.tf
@@ -4,8 +4,11 @@ module "ec2-windows" {
 
   hostname  = "testsrv456"
   env       = "TEST"
-  vpc_id    = aws_vpc.vpc-us-east-1.id
-  subnet_id = aws_subnet.public-us-east-1[0]
+  # vpc_id    = aws_vpc.vpc-us-east-1.id
+  # subnet_id = aws_subnet.public-us-east-1[0]
+  vpc_id = "vpc-0049e1e522019fd1e"
+  subnet_id = "subnet-0e0b88f9d0e85c39e"
+
   depends_on = [
     aws_kms_key.key
   ]
