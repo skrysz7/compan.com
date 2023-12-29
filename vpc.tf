289d236e96777461de136ffb390dc7bdac0fce57
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-12-29 16:21:08 +0100

    123

--- vpc.tf
+++ vpc.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_endpoint" "ssm" {
   service_name        = "com.amazonaws.us-east-1.ssm"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
-  subnet_ids          = [aws_subnet.private-us-east-1[*].id]
+  subnet_ids          = [aws_subnet.private-us-east-1[0].id,aws_subnet.private-us-east-1[1].id]
   private_dns_enabled = true
 }
 
@@ -24,6 +24,6 @@ resource "aws_vpc_endpoint" "ssmmessages" {
   service_name        = "com.amazonaws.us-east-1.ssmmessages"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [aws_security_group.vpc_endpoint_sg.id]
-  subnet_ids          = [aws_subnet.private-us-east-1[*].id]
+  subnet_ids          = [aws_subnet.private-us-east-1[0].id,aws_subnet.private-us-east-1[1].id]
   private_dns_enabled = true
 }
