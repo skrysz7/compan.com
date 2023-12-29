2d37e85d872fcb4f6a62e571a2b03cc1e293e1df
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2023-12-29 21:30:53 +0100

    123

--- test-ec2.tf
+++ test-ec2.tf
@@ -1,18 +1,18 @@
-module "ec2-windows" {
-  source  = "app.terraform.io/compan/ec2-windows/aws"
-  version = "1.0.16"
+# module "ec2-windows" {
+#   source  = "app.terraform.io/compan/ec2-windows/aws"
+#   version = "1.0.16"
 
-  hostname = "testsrv456"
-  env      = "TEST"
-  # vpc_id    = aws_vpc.vpc-us-east-1.id
-  # subnet_id = aws_subnet.public-us-east-1[0]
-  vpc_id    = "vpc-0049e1e522019fd1e"
-  subnet_id = "subnet-0e0b88f9d0e85c39e"
-  extra_sgs = [aws_security_group.vpc_endpoint_sg.id]
+#   hostname = "testsrv456"
+#   env      = "TEST"
+#   # vpc_id    = aws_vpc.vpc-us-east-1.id
+#   # subnet_id = aws_subnet.public-us-east-1[0]
+#   vpc_id    = "vpc-0049e1e522019fd1e"
+#   subnet_id = "subnet-0e0b88f9d0e85c39e"
+#   extra_sgs = [aws_security_group.vpc_endpoint_sg.id]
 
-  depends_on = [
-    aws_kms_key.key
-  ]
-  kms_key = aws_kms_alias.kms_alias.name
-  #   is_migrated = false
-} 
+#   depends_on = [
+#     aws_kms_key.key
+#   ]
+#   kms_key = aws_kms_alias.kms_alias.name
+#   #   is_migrated = false
+# } 
