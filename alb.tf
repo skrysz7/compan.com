ad503c0d735413968101e5fa9cf30216a4875127
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-01-30 13:17:31 +0100

    123

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "test" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [aws_security_group.vpc_endpoint_sg.id]
-  subnets            = [aws_subnet.private-us-east-1]
+  subnets            = [aws_subnet.private-us-east-1[*]]
 
   enable_deletion_protection = false
 
