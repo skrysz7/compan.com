394095b746bb00d76976a44bbc225406be9a91a9
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-01-31 15:50:31 +0100

    123

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
   public_subnet1b = [for subnet in aws_subnet.public-us-east-1 : subnet.id if subnet.availability_zone == "us-east-1b"]
 
   private_subnets = {
-      private_subnet1a = [for subnet in aws_subnet.private-us-east-1 : subnet.id if subnet.availability_zone == "us-east-1a"]
-      private_subnet1b = [for subnet in aws_subnet.private-us-east-1 : subnet.id if subnet.availability_zone == "us-east-1b"]
+    private_subnet1a = { id = "subnet-0e0b88f9d0e85c39e"}
+    private_subnet1b = { id = "subnet-08d2a03679d13feaf"}
   }
 }
