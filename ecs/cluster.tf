9dfbe531368de8c0c03c21b8b80df83f08d2ca8b
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-03-12 09:52:43 +0100

    123

--- ecs/cluster.tf
+++ ecs/cluster.tf
@@ -8,6 +8,7 @@ resource "aws_ecs_cluster" "boutique" {
 
   configuration {
     execute_command_configuration {
+        logging    = "OVERRIDE"
         log_configuration {
             cloud_watch_encryption_enabled = true
             cloud_watch_log_group_name     = aws_cloudwatch_log_group.boutique_lg.name
