880f8cb8150ed8a7ce9f29100851d03b723eacb9
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-03-12 17:18:44 +0100

    123

--- route_tables.tf
+++ route_tables.tf
@@ -39,11 +39,11 @@ resource "aws_route" "private_route" {
 }
 
 resource "aws_route_table_association" "private-1a" {
-  subnet_id      = local.private_subnets.private_subnet1a
+  subnet_id      = local.private_subnets.private_subnet1a.id
   route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_route_table_association" "private-1b" {
-  subnet_id      = local.private_subnets.private_subnet1b
+  subnet_id      = local.private_subnets.private_subnet1b.id
   route_table_id = aws_route_table.private_route_table.id
 }
