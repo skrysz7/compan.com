1bdb950fa15d66c3675fe6317415c73961b86cb1
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-03-12 07:55:37 +0100

    create ecs cluster

---
+++ ecs/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "admin" {
+  name                 = "ECS_AdministratorAccess"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
---
+++ ecs/variables.tf
@@ -0,0 +1,4 @@
+variable "service_connect_namespace" {
+  default = "boutique"
+  type    = string
+}
