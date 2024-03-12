eefbfa2a07ca3f927e6c3d1f722c8cc479db2725
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-03-12 16:13:35 +0100

    123

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -5,8 +5,10 @@ variable "service_connect_namespace" {
 
 variable "public_subnet1_id" {
   description = "ID of subnet1"
+  default = aws_subnet.public-us-east-1[0].id
 }
 
 variable "public_subnet2_id" {
   description = "ID of subnet2"
+  default = aws_subnet.public-us-east-1[1].id
 }
--- variables.tf
+++ variables.tf
@@ -29,10 +29,12 @@ variable "public_subnet_names" {
 
 variable "public_subnet1_id" {
   description = "ID of subnet1"
+  default = aws_subnet.public-us-east-1[0].id
 }
 
 variable "public_subnet2_id" {
   description = "ID of subnet2"
+  default = aws_subnet.public-us-east-1[1].id
 }
 
 variable "private_subnet_names" {
