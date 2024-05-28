a5d36fe72c8f5921e6251665ae49f75f0966bda3
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-05-28 16:56:06 +0200

    123

--- ecs.tf
+++ ecs.tf
@@ -1,8 +1,8 @@
-module "ecs" {
-    source = "./ecs"
-    public_subnet1_id   = element(aws_subnet.public-us-east-1[*].id, 0)
-    public_subnet2_id   = element(aws_subnet.public-us-east-1[*].id, 1)
-    private_subnet1_id  = element(aws_subnet.private-us-east-1[*].id, 0)
-    private_subnet2_id  = element(aws_subnet.private-us-east-1[*].id, 1)
-    vpc_id              = aws_vpc.vpc-us-east-1.id
-}
+# module "ecs" {
+#     source = "./ecs"
+#     public_subnet1_id   = element(aws_subnet.public-us-east-1[*].id, 0)
+#     public_subnet2_id   = element(aws_subnet.public-us-east-1[*].id, 1)
+#     private_subnet1_id  = element(aws_subnet.private-us-east-1[*].id, 0)
+#     private_subnet2_id  = element(aws_subnet.private-us-east-1[*].id, 1)
+#     vpc_id              = aws_vpc.vpc-us-east-1.id
+# }
