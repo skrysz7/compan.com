4c399744ce71058ffe8a448059885834856d1801
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-06-18 11:53:41 +0200

    123

---
+++ docker_test.tf
@@ -0,0 +1,45 @@
+locals {
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/////////////  Substitute below values to match your AWS account, region & profile //////////////////////////////////////////////////////////////////////////////////////////////
+  aws_account = "342023131128"   # AWS account
+  aws_region  = "us-east-1"      # AWS region
+  aws_profile = "default" # AWS profile
+ ///////////////////////////////////////////////////////////////////////////////////////////// 
+  ecr_reg   = "${local.aws_account}.dkr.ecr.${local.aws_region}.amazonaws.com" # ECR docker registry URI
+  ecr_repo  = "demo"                                                           # ECR repo name
+  image_tag = "latest"                                                         # image tag
+
+  dkr_img_src_path = "${path.module}/docker-src"
+  dkr_img_src_sha256 = sha256(join("", [for f in fileset(".", "${local.dkr_img_src_path}/**") : file(f)]))
+
+  dkr_build_cmd = <<-EOT
+        docker build -t ${local.ecr_reg}/${local.ecr_repo}:${local.image_tag} \
+            -f ${local.dkr_img_src_path}/Dockerfile .
+
+        aws --profile ${local.aws_profile} ecr get-login-password --region ${local.aws_region} | \
+            docker login --username AWS --password-stdin ${local.ecr_reg}
+
+        docker push ${local.ecr_reg}/${local.ecr_repo}:${local.image_tag}
+    EOT
+
+}
+
+variable "force_image_rebuild" {
+  type    = bool
+  default = false
+}
+
+# local-exec for build and push of docker image
+resource "null_resource" "build_push_dkr_img" {
+  triggers = {
+    detect_docker_source_changes = var.force_image_rebuild == true ? timestamp() : local.dkr_img_src_sha256
+  }
+  provisioner "local-exec" {
+    command = local.dkr_build_cmd
+  }
+}
+
+output "trigged_by" {
+  value = null_resource.build_push_dkr_img.triggers
+}
--- null_resource.tf
+++ null_resource.tf
@@ -1,47 +1,47 @@
-# provider "aws" {
-#   region = "us-east-1"  # Change to your preferred region
+# # provider "aws" {
+# #   region = "us-east-1"  # Change to your preferred region
+# # }
+
+# # provider "null" {
+# #   version = "~> 3.0"
+# # }
+
+# resource "aws_ecr_repository" "nexus_repo" {
+#   name = "nexus-repo"
 # }
 
-# provider "null" {
-#   version = "~> 3.0"
+# resource "null_resource" "pull_and_push_image" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       #!/bin/bash
+#       set -e
+
+#       ECR_URL=${aws_ecr_repository.nexus_repo.repository_url}
+
+#       # Get the latest tag of the nexus3 image from Docker Hub
+#       LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/sonatype/nexus3/tags/?page_size=1 | jq -r '.results[0].name')
+
+#       echo "Latest tag of sonatype/nexus3: $LATEST_TAG"
+
+#       # Log in to AWS ECR
+#       $(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL)
+
+#       # Pull the latest image from Docker Hub
+#       docker pull sonatype/nexus3:$LATEST_TAG
+
+#       # Tag the image for ECR
+#       docker tag sonatype/nexus3:$LATEST_TAG $ECR_URL:latest
+
+#       # Push the image to ECR
+#       docker push $ECR_URL:latest
+
+#       echo "Pushed $ECR_URL:latest"
+#     EOT
+#   }
+
+#   depends_on = [aws_ecr_repository.nexus_repo]
 # }
 
-resource "aws_ecr_repository" "nexus_repo" {
-  name = "nexus-repo"
-}
-
-resource "null_resource" "pull_and_push_image" {
-  provisioner "local-exec" {
-    command = <<EOT
-      #!/bin/bash
-      set -e
-
-      ECR_URL=${aws_ecr_repository.nexus_repo.repository_url}
-
-      # Get the latest tag of the nexus3 image from Docker Hub
-      LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/sonatype/nexus3/tags/?page_size=1 | jq -r '.results[0].name')
-
-      echo "Latest tag of sonatype/nexus3: $LATEST_TAG"
-
-      # Log in to AWS ECR
-      $(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URL)
-
-      # Pull the latest image from Docker Hub
-      docker pull sonatype/nexus3:$LATEST_TAG
-
-      # Tag the image for ECR
-      docker tag sonatype/nexus3:$LATEST_TAG $ECR_URL:latest
-
-      # Push the image to ECR
-      docker push $ECR_URL:latest
-
-      echo "Pushed $ECR_URL:latest"
-    EOT
-  }
-
-  depends_on = [aws_ecr_repository.nexus_repo]
-}
-
-output "repository_url" {
-  value = aws_ecr_repository.nexus_repo.repository_url
-}
+# output "repository_url" {
+#   value = aws_ecr_repository.nexus_repo.repository_url
+# }
