74770c4f0642a01e45121fedb7e5f1cdef12a0c4
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-06-20 14:31:48 +0200

    123

--- docker_test.tf
+++ docker_test.tf
@@ -15,31 +15,31 @@ terraform {
 #   region = "us-west-2" # Update to your preferred region
 # }
 
-resource "aws_ecr_repository" "this" {
-  name = "my-app"
-}
+# resource "aws_ecr_repository" "this" {
+#   name = "my-app"
+# }
 
-data "aws_caller_identity" "this" {}
+# data "aws_caller_identity" "this" {}
 
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
 
-data "aws_ecr_authorization_token" "temporary" {
-  registry_id = data.aws_caller_identity.this.account_id
-}
+# data "aws_ecr_authorization_token" "temporary" {
+#   registry_id = data.aws_caller_identity.this.account_id
+# }
 
-provider "dockerless" {
-  registry_auth = {
-    "${data.aws_caller_identity.this.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com" = {
-      username = data.aws_ecr_authorization_token.temporary.user_name
-      password = data.aws_ecr_authorization_token.temporary.password
-    }
-  }
-}
+# provider "dockerless" {
+#   registry_auth = {
+#     "${data.aws_caller_identity.this.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com" = {
+#       username = data.aws_ecr_authorization_token.temporary.user_name
+#       password = data.aws_ecr_authorization_token.temporary.password
+#     }
+#   }
+# }
 
-resource "dockerless_remote_image" "alpine_latest" {
-  source = "alpine:latest"
-  target = "${aws_ecr_repository.this.repository_url}:latest"
-}
+# resource "dockerless_remote_image" "alpine_latest" {
+#   source = "alpine:latest"
+#   target = "${aws_ecr_repository.this.repository_url}:latest"
+# }
 
 #############
 # resource "aws_db_snapshot" "test" {
