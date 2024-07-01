921155ddb9fab19d32a93ae287007c6aee3306b1
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-07-01 12:31:18 +0200

    123

--- ecs2/docker_test.tf
+++ ecs2/docker_test.tf
@@ -3,7 +3,7 @@
 # }
 
 resource "aws_ssm_parameter" "rds_db_snapshot" {
-  count  = var.rollback ? 1 : 0
+  count  = var.rollback ? 0 : 1
   name   = "/nexus/rds-db-snapshot/name"
   type   = "SecureString"
   value  = aws_db_snapshot.create_rds_snapshot[0].db_snapshot_identifier
@@ -46,7 +46,7 @@ locals {
 }
 
 resource "aws_db_snapshot" "create_rds_snapshot" {
-  count                  = var.rollback ? 1 : 0
+  count                  = var.rollback ? 0 : 1
   db_instance_identifier = "test"
   db_snapshot_identifier = local.snapshot_identifier
   lifecycle {
