65566a79b1f7ba58226fea128980e21f0370f505
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-07-01 12:41:16 +0200

    123

--- ecs2/variables.tf
+++ ecs2/variables.tf
@@ -7,5 +7,5 @@ variable "container_image_version" {
 variable "rollback" {
   description = "Flag to control the creation of the DB snapshot"
   type    = bool
-  default = false
+  default = true
 }
