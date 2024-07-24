493a8563f0d168ff07eaca7e48ad50c87771a1da
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2024-07-24 17:40:05 +0200

    0.4

--- ecs2/docker_test.tf
+++ ecs2/docker_test.tf
@@ -11,6 +11,21 @@ terraform {
     }
   }
 }
+data "aws_caller_identity" "this" {}
+
+data "aws_region" "current" {}
+
+data "aws_ecr_authorization_token" "temporary" {
+  registry_id = data.aws_caller_identity.this.account_id
+}
+provider "dockerless" {
+  registry_auth = {
+    "${data.aws_caller_identity.this.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com" = {
+      username = data.aws_ecr_authorization_token.temporary.user_name
+      password = data.aws_ecr_authorization_token.temporary.password
+    }
+  }
+}
 resource "dockerless_remote_image" "nginxdemos" {
   source = "nginxdemos/hello:${var.container_image_version}"
   target = "${var.container_ecr_url}:${var.container_image_version}"
